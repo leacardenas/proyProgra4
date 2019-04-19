2db2a0320b839143e6afc8cf6573fa421fb4ad6a
Fiorella Salgado <fiorella.salgado.rodriguez@est.una.ac.cr>
2019-04-19 00:21:08 -0600

    Conexion a la base
    Metodos del gestor para conectar a la base

--- Programacion4-Proyecto1/src/java/modelo/dao/GestorEstudiante.java
+++ Programacion4-Proyecto1/src/java/modelo/dao/GestorEstudiante.java
@@ -5,12 +5,35 @@
  */
 package modelo.dao;
 
+import cr.ac.database.managers.DBManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 /**
  *
  * @author fiore
  */
 public class GestorEstudiante {
     private static GestorEstudiante instancia = null;
+    
+    private static final String BASE_DATOS = "proy_progra4";
+    private static final String LOGIN = "root";
+    private static final String PASSWORD = "root";
+    
+    private static final String CMD_LISTAR
+            = "SELECT id, nrc,apellidos,nombre,secuencia,clave,ultimo_acceso,grupo_id "
+            + "FROM estudiante ORDER BY id ";
+    
+    private static final String CMD_VERIFICAR
+            = "SELECT id FROM estudiante "
+            + "WHERE id=? AND clave=? ";
+    
+    
+    private String UserLogin = " ";
     
     private GestorEstudiante(){
         
@@ -22,4 +45,104 @@ public class GestorEstudiante {
         }
         return instancia;
     }
+    
+    public ArrayList<Object[]> obtenerLista() {
+        ArrayList<Object[]> usuarios = new ArrayList<>();
+
+        // Abre una conexión a la base de datos y carga la lista de usuarios.
+        DBManager bd = null;
+        try {
+            bd = DBManager.getDBManager(DBManager.DB_MGR.MYSQL_SERVER);
+            Connection cnx = bd.getConnection(BASE_DATOS, LOGIN, PASSWORD);
+
+            Statement stm = cnx.createStatement();
+            ResultSet rs = stm.executeQuery(CMD_LISTAR);
+            int maxCols = rs.getMetaData().getColumnCount();
+            while (rs.next()) {
+                Object[] registro = new Object[maxCols];
+                for (int i = 0; i < maxCols; i++) {
+                    registro[i] = rs.getObject(i + 1);
+                }
+                usuarios.add(registro);
+            }
+        } catch (ClassNotFoundException
+                | IllegalAccessException
+                | InstantiationException
+                | SQLException e) {
+            e.printStackTrace(System.err);
+        } finally {
+            bd.closeConnection();
+        }
+
+        return usuarios;
+    }
+    
+    public static String listaUsuariosHTML(GestorEstudiante g) {
+        StringBuilder r = new StringBuilder();
+        ArrayList<Object[]> usuarios = g.obtenerLista();
+        if (usuarios.size() > 0) {
+            for (Object[] registro : usuarios) {
+                r.append("<tr>");
+                for (Object e : registro) {
+                    r.append("<td>");
+                    if (e != null) {
+                        r.append(e.toString());
+                    }else {
+                        r.append("(null)");
+                    }
+                    r.append("</td>");
+                }
+                r.append("</tr>");
+            }
+        } else {
+            r.append("<tr><td colspan=\"5\">(No hay registros en la base de datos.)</td></tr>");
+        }
+
+        return r.toString();
+    }
+    
+    public boolean verificarUsuario(String usuario, String clave) {
+
+        boolean encontrado = false;
+        DBManager bd = null;
+        try {
+            bd = DBManager.getDBManager(DBManager.DB_MGR.MYSQL_SERVER);
+            Connection cnx
+                    = bd.getConnection(BASE_DATOS, LOGIN, PASSWORD);
+
+            try (PreparedStatement stm = cnx.prepareStatement(CMD_VERIFICAR)) {
+                stm.clearParameters();
+                stm.setString(1, usuario);
+                stm.setString(2, clave);
+                ResultSet rs = stm.executeQuery();
+                encontrado = rs.next();
+            }
+
+        } catch (ClassNotFoundException
+                | IllegalAccessException
+                | InstantiationException
+                | SQLException e) {
+            e.printStackTrace(System.err);
+        } finally {
+            if (bd != null) {
+                bd.closeConnection();
+            }
+        }
+        return encontrado;
+    }
+
+    public String getUserLogin() {
+        return UserLogin;
+    }
+
+    public void setUserLogin(String UserLogin) {
+        this.UserLogin = UserLogin;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+    
 }
